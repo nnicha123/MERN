6e678eb9ec16323fdbe2e8ec0813fc627bf9ea4a
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-01 09:52:29 +0700

    finished the backend part using mongoDB, mongoDB atlas/cluster, postman and nodejs

--- mern-exercise/backend/server.js
+++ mern-exercise/backend/server.js
@@ -7,6 +7,10 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
+// Middlewares
+app.use(cors())
+app.use(express.json())
+
 const uri = process.env.ATLAS_URI
 mongoose.connect(uri, {useNewUrlParser:true, useCreateIndex:true})
 const connection = mongoose.connection
@@ -14,9 +18,11 @@ connection.once('open', () => {
     console.log('MongoDB database connection established successfully')
 })
 
-// Middlewares
-app.use(cors())
-app.use(express.json())
+const exercisesRouter = require('./routes/exercises')
+const usersRouter = require('./routes/users') 
+
+app.use('/exercises',exercisesRouter)
+app.use('/users',usersRouter)
 
 // Start server
 app.listen(port,()=> {
